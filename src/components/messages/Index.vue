175ff88edf5f92e6104da63de4f6662074b0841b
Richman Larry CLifford <larriche99@gmail.com>
2018-05-18 21:19:11 +0000

    Designed layout for messages page

--- src/components/messages/Index.vue
+++ src/components/messages/Index.vue
@@ -1,5 +1,53 @@
 <template>
     <div class="col-lg-10 col-md-10 col-xs-12">
-        <p>Hello World</p>
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 page-title">
+                <h1><span class="fa fa-comments-o"></span> Messages</h1>
+            </div>
+        </div>
+
+        <div class="row page-body">
+            <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 col-lg-offset-2 col-md-offset-2">
+                <message
+                    :direction="'incoming'"></message>
+                <message
+                    :direction="'outgoing'"></message>
+
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-xs-12">
+                            <div class="input-group">
+                                <input type="text" class="form-control chatbox" placeholder="Enter a keyword to search" name="search_term">
+
+                                <span class="input-group-btn">
+                                    <button type="submit" class="btn btn-lg btn-info" id="search-button">
+                                        SEND
+                                    </button>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </div>
     </div>
 </template>
+
+<script>
+import Message from './Message';
+
+export default {
+    components: {
+        Message
+    }
+}
+</script>
+
+<style scoped>
+.chatbox {
+    font-size: 1.4em;
+    height: 50px;
+}
+
+#search-button {
+    height: 50px;
+}
+</style>
---
+++ src/components/messages/Message.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="row message-container">
+        <div :class="messageClass">
+            <p>Hello World</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['direction'],
+
+    computed: {
+        messageClass() {
+            if (this.direction == 'incoming') {
+                return 'message incoming col-lg-6 col-md-6 col-xs-12';
+            } else {
+                return 'message outgoing col-lg-6 col-md-6 col-xs-12 col-lg-offset-6 col-md-offset-6';
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.message {
+    height: 50px;
+    padding: 10px;
+    color: white;
+    border-radius: 10px;
+    font-size: 1.3em;
+}
+
+.message-container {
+    margin-bottom: 30px;
+}
+
+.incoming {
+    background: green;
+}
+
+.outgoing {
+    background: lightseagreen;
+}
+</style>
