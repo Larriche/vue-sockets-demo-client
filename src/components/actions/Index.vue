9a537e3d1364d152b5cf4f64a31d23312574f6cb
Richman Larry CLifford <larriche99@gmail.com>
2018-05-23 13:01:14 +0000

    Implemented sending of user action statistics to server

--- src/components/actions/Index.vue
+++ src/components/actions/Index.vue
@@ -9,12 +9,15 @@
         <div class="row page-body">
             <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 col-lg-offset-2 col-md-offset-2">
                 <div class="form-group">
-                    <input type="radio">
+                    <input type="radio"
+                        data-name="radiobutton"
+                        @click="sendStatistic($event, 'radiobutton_clicked')">
                     <label>Radiobutton</label>
                 </div>
 
                 <div class="form-group">
-                    <select class="form-control">
+                    <select class="form-control"
+                        @change="sendStatistic($event, 'select_changed')">
                         <option>Select an item</option>
                         <option value="one">Option One</option>
                         <option value="two">Option Two</option>
@@ -22,17 +25,23 @@
                 </div>
 
                 <div class="form-group">
-                    <input type="checkbox">
+                    <input type="checkbox"
+                        data-name="checkbox1"
+                        @click="sendStatistic($event, 'checkbox_clicked')">
                     <label>Checkbox 1</label>
                 </div>
 
                 <div class="form-group">
-                    <input type="checkbox">
-                    <label>Checkbox 1</label>
+                    <input type="checkbox"
+                        data-name="checkbox2"
+                        @click="sendStatistic($event, 'checkbox_clicked')">
+                    <label>Checkbox 2</label>
                 </div>
 
                 <div class="form-group">
-                    <button class="btn btn-md btn-success">Action Button</button>
+                    <button class="btn btn-md btn-success"
+                        @click="sendStatistic($event, 'action_button_click')"
+                        @dblclick="sendStatistic($event, 'action_button_double_click')">Action Button</button>
                 </div>
 
                 <div class="form-group">
@@ -42,3 +51,41 @@
         </div>
     </div>
 </template>
+
+<script>
+// Vuex imports
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+    computed: {
+        ...mapGetters({
+            user: 'Auth/getUser',
+        })
+    },
+
+    methods: {
+        sendStatistic(event, action) {
+            let target = event.target;
+
+            if (action == 'checkbox_clicked') {
+                let $target = $(target);
+                let name = $target.data('name');
+
+                if ($target.is(':checked')) {
+                    action = name + '_checked';
+                } else {
+                    action = name + '_unchecked';
+                }
+            }
+
+            let statistic = {
+                type: 'event',
+                fromId: this.user.id,
+                action
+            };
+
+            this.$socket.emit('statistic', statistic);
+        }
+    }
+}
+</script>
